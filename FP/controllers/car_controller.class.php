43b9e8ed865e6b3d97e8d0b5348a17722ada0e11
J.M Winbush <jawonwinbush@gmail.com>
2022-11-17 04:16:25 -0500

    updated CarController class

--- FP/controllers/car_controller.class.php
+++ FP/controllers/car_controller.class.php
@@ -2,7 +2,7 @@
 
 /* Author: Matt Cool, Steven Casada, Jawon Winbush
 * Date: 11/16/22
- * user_controller.class.php
+ * car_controller.class.php
  * perform the functions of the various class pages
  */
 
