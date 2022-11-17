3b2b5fadd84264c44a3d34300586d355addff381
J.M Winbush <jawonwinbush@gmail.com>
2022-11-17 02:45:38 -0500

    CarController class

---
+++ FP/controllers/car_controller.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Author: Matt Cool, Steven Casada, Jawon Winbush
+* Date: 11/16/22
+ * user_controller.class.php
+ * perform the functions of the various class pages
+ */
+
+class CarController {
+
+
+}
